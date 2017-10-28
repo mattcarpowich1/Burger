517db35462be71a9cfb2c45a87923da0b2f916b2
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2017-10-27 20:24:40 -0700

    fixing bug

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 let connection;
 
 if (process.env.JAWSDB_URL) {
-	connection = mysqlcreateConnection(process.env.JAWSDB_URL);
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
 		host: "127.0.0.1",
